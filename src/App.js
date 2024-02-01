a501bfc2c66eae1a7dd481e50c3c25abc8e48808
Brian A <brian.ahlers@gmail.com>
2024-01-31 17:27:04 -0800

    set up file and folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from 'react';
+import { Card } from './components/card/Card';
+import s from './global.css'
+
 function App() {
     return (
-        <div>
-        <p>Hi there!</p>
+        <div className ="">
+        <p>I am in the app</p>
+        <Card />
         </div>
     );
 }
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,14 @@
+import { Body } from "../body/Body";
+import { Footer } from "../footer/Footer";
+import { Header } from "../header/Header";
+
+export function Card() {
+  return (
+    <div>
+      Card div here below me are the components 
+        <Header />
+      <Body />
+        <Footer />
+    </div>
+  );
+}
